a2e242bb780ebc66f0f456140c6d8fed6f593428
Andrew Stanford-Jason <andrew@xmos.com>
2022-03-29 17:11:42 +0100

    Update sortPixels.js

--- scripts/sortPixels.js
+++ scripts/sortPixels.js
@@ -8,6 +8,7 @@ const writeFile = promisify(fs.writeFile);
 
 const pixelFilePath = path.join('_data', 'pixels.json');
 
+
 readFile(pixelFilePath, { encoding: 'utf8' })
   .then(pixelFileData => {
     const pixels = JSON.parse(pixelFileData);
